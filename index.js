5b99ce736930b7cb9301ce389abf651330c66859
CodeByFatima <fatimamadeit@gmail.com>
2026-02-17 02:10:27 +0500

    Datatype conversion

---
+++ index.js
@@ -0,0 +1,11 @@
+let score = "33";
+console.log(typeof score) //type is string
+
+// node index.js
+
+let valInNumber = Number(score)
+console.log(typeof valInNumber) //type is now a number
+
+let isLoggedIn = 0;
+let convertIt = Boolean(isLoggedIn)
+console.log(convertIt) //converted into boolean
