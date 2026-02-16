69a93c424f7dbd2b61703b47aeb4781dacc88503
CodeByFatima <fatimamadeit@gmail.com>
2026-02-17 02:50:33 +0500

    comparison operators

---
+++ comparison.js
@@ -0,0 +1,9 @@
+// comparison operators
+console.log(1 > 9)
+console.log(1 >= 9)
+console.log(1 < 9)
+console.log(1 <= 9)
+console.log(1 != 9)
+console.log(2 == "2") //returns true because the string "2" is converted to the number 2 before comparison
+console.log(2 === "2") // returns false because, even though the values are the same, the types (number and string) are different
+
